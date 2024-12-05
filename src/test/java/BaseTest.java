30c6858821fab1f30ec5dac0eacbddb3cf536121
Salma <selma.1390@gmail.com>
2024-12-04 20:08:21 -0500

    Working on BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,6 @@
 import config.ConfigManager;
 import config.DriverFactory;
+import org.openqa.selenium.WebDriver;
 
 import java.lang.invoke.ConstantBootstraps;
 
@@ -7,7 +8,9 @@ public class BaseTest {
     public static void main(String[] args) {
 
         ConfigManager.printAllProperties();
-        DriverFactory.initializeWebDriver();
+        WebDriver driver = DriverFactory.initializeWebDriver();
+        driver.get(ConfigManager.getProperty("baseUrl"));
+        driver.quit();
 
 
 
